6b5ce49f2c45548a3cc6ba5a736e7f8565465065
nrsabit <mailbox.sabit@gmail.com>
2023-05-04 20:28:01 +0600

    enviornment variable added

--- src/components/pages/NotFound/NotFound.jsx
+++ src/components/pages/NotFound/NotFound.jsx
@@ -6,9 +6,9 @@ const NotFound = () => {
     return (
         <div>
             <Header></Header>
-            <div>
-                <h2 className='orange text-center fs-1'>Page Not Found</h2>
-            <img className='text-center' src="https://img.freepik.com/free-vector/page-found-concept-illustration_114360-1869.jpg?w=996&t=st=1683190893~exp=1683191493~hmac=eb7bf60e4e272ce0ed1322e2b4cd0460b1d2ca2f20ab7046e20df272465dbd9b" alt="" />
+            <div className='d-flex flex-column justify-content-center align-items-center'>
+                <h2 className='orange text-center fs-1 mt-5'>Page Not Found</h2>
+                <img style={{width: '50%', height: '600px'}} src="https://img.freepik.com/free-vector/page-found-concept-illustration_114360-1869.jpg?w=996&t=st=1683190893~exp=1683191493~hmac=eb7bf60e4e272ce0ed1322e2b4cd0460b1d2ca2f20ab7046e20df272465dbd9b" alt="" />
             </div>
             <Footer></Footer>
         </div>
