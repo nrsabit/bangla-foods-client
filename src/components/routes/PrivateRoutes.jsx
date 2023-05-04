f9fea0885f1b6347ec2da6f5fda1b06c3347b93f
nrsabit <mailbox.sabit@gmail.com>
2023-05-05 00:14:03 +0600

    private route reloading stopped

--- src/components/routes/PrivateRoutes.jsx
+++ src/components/routes/PrivateRoutes.jsx
@@ -1,14 +1,18 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../providers/AuthProvider';
 import { Navigate, useLocation } from 'react-router-dom';
+import { Spinner } from 'react-bootstrap';
 
-const PrivateRoutes = ({children}) => {
-    const {user} = useContext(AuthContext);
+const PrivateRoutes = ({ children }) => {
+    const { user, loader } = useContext(AuthContext);
     const location = useLocation();
-    if(user){
+    if (loader) {
+        return <div className='mt-5 d-flex justify-content-center'><Spinner animation="grow"></Spinner></div>
+    }
+    if (user) {
         return children;
-    }else{
-      return <Navigate to="/login" state={{from: location}} replace></Navigate>
+    } else {
+        return <Navigate to="/login" state={{ from: location }} replace></Navigate>
     }
 };
 
