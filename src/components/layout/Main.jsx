80eb17d224e420b396ca105fcd4f2bb6b40bb55a
nrsabit <mailbox.sabit@gmail.com>
2023-05-04 22:54:36 +0600

    spinner added

--- src/components/layout/Main.jsx
+++ src/components/layout/Main.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Header from '../others/Header/Header';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 import Footer from '../others/Footer/Footer';
+import { Container, Spinner } from 'react-bootstrap';
 
 const Main = () => {
+    const spinner = <Spinner animation="grow"></Spinner>
+    const navigation = useNavigation();
     return (
         <div>
             <Header></Header>
-            <Outlet></Outlet>
+            {navigation.state === 'loading' ? <Container><div className='text-center mt-5'>{spinner}</div></Container> : <Outlet></Outlet>}
             <Footer></Footer>
         </div>
     );
